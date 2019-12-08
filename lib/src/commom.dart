6b177a865e2fc81e1997a747269e72768ff4088f
ahyangnb <zonggeu@gmail.com>
2019-12-08 23:07:44 +0800

    commit add function

--- lib/src/commom.dart
+++ lib/src/commom.dart
@@ -15,11 +15,24 @@ Future<dynamic> routePushName(String name) {
   return navGK.currentState.pushNamed(name);
 }
 
+Future<dynamic> maybePop([result]) {
+  return navGK.currentState.maybePop(result ?? '');
+}
+
+Future<dynamic> popAndPushNamed(name, [result]) {
+  return navGK.currentState.popAndPushNamed(name, result: result ?? '');
+}
+
 Future<dynamic> pushAndRemoveUntil(Widget page,
     [RouterType type = RouterType.cupertino]) {
   Route route = routerUtil(type: type, widget: page);
   return navGK.currentState
       .pushAndRemoveUntil(route, (Route<dynamic> route) => false);
+}
+
+Future<dynamic> pushNamedAndRemoveUntil(String newRouteName) {
+  return navGK.currentState
+      .pushNamedAndRemoveUntil(newRouteName, (Route<dynamic> route) => false);
 }
 
 Future<dynamic> pushReplacement(Widget page,
@@ -28,7 +41,18 @@ Future<dynamic> pushReplacement(Widget page,
   return navGK.currentState.pushReplacement(route);
 }
 
-
 void popToRootPage() {
   navGK.currentState.popUntil(ModalRoute.withName('/'));
+}
+
+void popUntil(RoutePredicate predicate) {
+  return navGK.currentState.popUntil(predicate);
+}
+
+bool pop([result]) {
+  return navGK.currentState.pop(result ?? '');
+}
+
+bool canPop() {
+  return navGK.currentState.canPop();
 }
