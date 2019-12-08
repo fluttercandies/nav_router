fb6696e4e6739b0eccb28383fa377013039ff650
ahyangnb <zonggeu@gmail.com>
2019-12-08 16:56:09 +0800

    commit push init

--- lib/nav_router.dart
+++ lib/nav_router.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+export 'util/route.dart';
 
 class NavRouter {
   static const MethodChannel _channel =
---
+++ lib/util/route.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+final navGK = new GlobalKey<NavigatorState>();
+
+Future<dynamic> routePush(Widget widget) {
+  final route = new CupertinoPageRoute(
+    builder: (BuildContext context) => widget,
+    settings: new RouteSettings(
+      name: widget.toStringShort(),
+      isInitialRoute: false,
+    ),
+  );
+  return navGK.currentState.push(route);
+}
