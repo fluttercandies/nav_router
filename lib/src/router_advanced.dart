051e6ba05c47d19874540b003f0a4d8dc73dbd2f
ahyangnb <zonggeu@gmail.com>
2020-03-04 18:46:39 +0800

    commit advance des

--- lib/src/router_advanced.dart
+++ lib/src/router_advanced.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nav_router/all_routes.dart';
 
+/// This is an advanced usage of sliding routing.
+/// Others can be added later.
+/// Currently only this one is available.
 Route advanceSlide({exitPage, enterPage}) {
   return AdvanceSlideRoute(enterPage: enterPage, exitPage: exitPage);
 }
