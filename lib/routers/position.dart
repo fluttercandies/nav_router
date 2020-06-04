2cb54a320ea38b6f97d7ecd719863264f93d8161
ahyangnb <zonggeu@gmail.com>
2020-06-04 14:40:26 +0800

    commit PositionedTransition

---
+++ lib/routers/position.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+/// [Route animation effect]
+/// Transparent transition mode route jump animation.
+///
+class PositionRoute extends PageRouteBuilder {
+  final Widget page;
+
+  PositionRoute({this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              PositionedTransition(
+            rect: Tween<RelativeRect>(
+              begin: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 10.0),
+              end: RelativeRect.fromLTRB(0.0, 10.0, 0.0, 0.0),
+            ).animate(
+                CurvedAnimation(parent: animation, curve: Curves.bounceIn)),
+            child: child,
+          ),
+        );
+}
